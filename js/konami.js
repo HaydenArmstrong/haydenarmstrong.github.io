b1dd0b2b842a3ea1016c5aef07d0a702e4e572a6
HaydenArmstrong <haydenninja123@gmail.com>
2017-08-26 16:15:43 -0600

    Update konami.js

--- js/konami.js
+++ js/konami.js
@@ -38,7 +38,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 function activateCheats() {
-  document.body.style.backgroundImage = "url('images/ramort.gif')";
+  document.body.style.backgroundImage = "url('ramort.gif')";
 
   alert("cheats activated");
 }
